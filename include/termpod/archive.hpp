6a983e126813a85d189c7b22372ba6e5f67f4d05
Jon Daniel <jopadan@mailfence.com>
2024-11-03 22:58:11 +0100

    move checksum to archive

--- include/termpod/archive.hpp
+++ include/termpod/archive.hpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <sys/stat.h>
 #include <utime.h>
+#include <crcle/crcle.hpp>
 
 namespace tr::pod
 {
@@ -59,6 +60,72 @@ namespace tr::pod
 		depend = 5,
 		audit  = 6,
 	};
+
+	/*
+	checksum: crc32::mpeg2
+	============================================================================
+	| POD2 : offset=sizeof(ident)+sizeof(checksum) count=sizeof(file  )-offset |
+	| POD3 : offset=sizeof(ident)+sizeof(checksum) count=sizeof(header)-offset |
+	| POD4 : offset=sizeof(ident)+sizeof(checksum) count=sizeof(header)-offset |
+	| POD5 : offset=sizeof(ident)+sizeof(checksum) count=sizeof(header)-offset |
+	| EPD  : offset=sizeof(header)                 count=sizeof(file  )-offset |
+	============================================================================
+	checksum: none
+	============================================================================
+	| POD6 : offset=sizeof(header)                 count=sizeof(file  )-offset |
+	| POD1 : offset=sizeof(header)=4+80            count=sizeof(file  )-offset |
+	============================================================================
+	*/
+	const std::pair<uint32_t, enum section> range[last] =
+	{
+		{ 0u                                                                             , section::none   },
+		{ sizeof(tr::archive<pod1>::header)                                              , section::file   },
+		{ sizeof(tr::archive<pod2>::header::ident) + sizeof(tr::pod<2>::header::checksum), section::file   },
+		{ sizeof(tr::archive<pod3>::header::ident) + sizeof(tr::pod<3>::header::checksum), section::header },
+		{ sizeof(tr::archive<pod4>::header::ident) + sizeof(tr::pod<4>::header::checksum), section::header },
+		{ sizeof(tr::archive<pod5>::header::ident) + sizeof(tr::pod<5>::header::checksum), section::header },
+		{ sizeof(tr::archive<pod6>::header)                                              , section::file   },
+		{ sizeof(tr::archive<epd>::header)                                               , section::file   },
+		{ sizeof(tr::archive<epd1>::header)                                              , section::file   },
+		{ sizeof(tr::archive<epd2>::header)                                              , section::file   },
+	};
+
+	template<enum version version>
+	constexpr inline size_t section_size(size_t size)
+	{
+		switch(range[version].second)
+		{
+			case section::entry:
+			case section::file:
+				return size;
+			case section::header:
+				return sizeof(tr::archive<version>::header);
+			case section::none:
+			default:
+				return 0;
+		}
+	}
+
+	template<enum version version>
+	constexpr inline size_t section_offset()
+	{
+		switch(range[version].second)
+		{
+			case section::header:
+			case section::file:
+				return range[version].first;
+			case section::entry:
+			case section::none:
+			default:
+				return 0;
+		}
+	}
+
+	template<enum version version>
+	constexpr inline uint32_t checksum(uint8_t* buf, size_t size)
+	{
+		return crc32::mpeg2::compute(buf + section_offset<version>(), section_size<version>(size) - section_offset<version>());
+	}
 
 	namespace audit
 	{
